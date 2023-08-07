19538471260b301bb12237c1428ffdba149a1bb9
Oleh Kulbaba <elatedexorcist@gmail.com>
2023-08-08 02:03:43 +0300

    Initial commit

---
+++ src/main/java/com/example/identatask/IdentaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.identatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentaTaskApplication.class, args);
+    }
+
+}
